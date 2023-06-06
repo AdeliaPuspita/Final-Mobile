d52431aa5fe5c29b3c29049c6bc3a636f511c1a6
adeliapuspita <adhepuspita123@gmail.com>
2023-06-06 15:35:55 +0800

    first commit

---
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView ivMovie, ivTv, ivFav;
+    private FragmentManager fragmentManager;
+    TextView tvToolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivMovie = findViewById(R.id.iv_movies);
+        ivTv = findViewById(R.id.iv_tv);
+        ivFav = findViewById(R.id.iv_fav);
+        tvToolbar = findViewById(R.id.tv_toolbar);
+
+        fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentByTag(MovieFragment.class.getSimpleName());
+        if (!(fragment instanceof  MovieFragment)) {
+            navigateFragment(new MovieFragment());
+        }
+
+        ivMovie.setOnClickListener(v -> {
+            navigateFragment(new MovieFragment());
+        });
+
+        ivTv.setOnClickListener(v -> {
+            navigateFragment(new TvFragment());
+        });
+
+        ivFav.setOnClickListener(v -> {
+            navigateFragment(new FavoriteFragment());
+        });
+
+    }
+
+    public void navigateFragment (Fragment fragment) {
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_main,fragment, fragment.getClass().getSimpleName())
+                .commit();
+
+        //komdisi utk cek fragment skrg
+        if (fragment instanceof MovieFragment) {
+            tvToolbar.setText("Movie");
+        }
+        if (fragment instanceof TvFragment) {
+            tvToolbar.setText("Tv Shows");
+        }
+        if (fragment instanceof FavoriteFragment) {
+            tvToolbar.setText("Favorie");
+        }
+
+    }
+}
